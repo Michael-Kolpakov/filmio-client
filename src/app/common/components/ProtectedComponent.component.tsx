b74dba4ecb079c62a3cf9dc067d73ad44dd62ebc
Michael-Kolpakov <93143249+Mikhail-Kolpakov@users.noreply.github.com>
2025-01-03 17:28:54 +0200

    Made it easier to determine whether a user is authorized or not

--- src/app/common/components/ProtectedComponent.component.tsx
+++ src/app/common/components/ProtectedComponent.component.tsx
@@ -1,15 +1,14 @@
 import { observer } from "mobx-react-lite";
 import React, { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
-import useMobx, {store} from "../../stores/root-store";
+import useMobx from "../../stores/root-store";
 import { FRONTEND_ROUTES } from "../constants/frontend-routes.constants";
 
 const ProtectedComponent: React.FC<{ children: ReactNode }> = observer(({ children }) => {
   const { usersStore } = useMobx();
-  const isLoggedIn = usersStore.isLoggedIn();
+  const isLoggedIn = usersStore.getIsUserLoggedIn();
 
   if (!isLoggedIn) {
-    void store.usersStore.logout();
     return <Navigate to={FRONTEND_ROUTES.MAIN.REGISTRATION} />;
   }
 
